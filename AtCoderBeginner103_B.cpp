cabc3f5f5bee8488ef5f0698319801bbc8a0d285
rurito <rurito.blog@gmail.com>
2018-07-22 02:01:50 +0900

    他の人のコードを参考にしました。

--- AtCoderBeginner103_B.cpp
+++ AtCoderBeginner103_B.cpp
@@ -1,44 +1,20 @@
 #include<iostream>
-#include<vector>
 #include <string>
-#include <sstream>
 
-std::vector<std::string> array1;
-std::vector<std::string> array2;
-std::stringstream ss{s};
+using namespace std;
 
 int main() {
-	std::string mozi;
-	array1 = std::vector<std::string>(100);
+        string boxA,boxB;
 
-        while (std::getline(ss, mozi)) {
-          array1.push_back(mozi);
-        }  
+	cin >> boxA >> boxB;
 
-        std::size_t size = array1.size();
-	array2 = std::vector<std::string>(100);
-       while (std::getline(ss, mozi)) {
-          array2.push_back(mozi);
-        }  
-
-
-
-	int check = 0;
-	bool flag = true;
-		for(int n = 0;n < size;n++) {
-			if(array1[n] != array2[n]) {
-				check++;
-				        array1.insert(array1.begin(),array1[size]);
-                                        array1.pop_back();
-					flag = false;
-					continue;
-			}
-			flag = true;
-			
+	for(int n = 0;n < boxA.size();n++) {
+		if(boxA == boxB) {
+		cout << "Yes" << "\n";
+		return 0;
 		}
-		if(flag) {
-			std::cout << "Yes" << "\n";
-		}else{
-			std::cout << "No" << "\n";
-		}
+		boxA = boxA[boxA.size()-1] + boxA.substr(0,boxA.size()-1);
+	}
+		cout << "No" << "\n";
+	return 0;
 }
