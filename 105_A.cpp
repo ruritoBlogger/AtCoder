03f26879765830a6750575c26c71d48dd83d08d7
ruritoblogger <rurito.blog@gmail.com>
2019-04-14 02:16:08 +0900

    change name

--- 105_A.cpp
+++ 105_A.cpp
@@ -1,16 +1,14 @@
-#include<bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
 
-#define ll long long
-#define INF 99999999
-#define rep(i, n) for(int i = 0; i < (int)(n); i++)
-#define all(x) (x).begin(),(x).end()
-
-int main()
-{
-    int n,k;
-    cin >> n >> k;
-
-    if(n%k==0)cout << "0" << endl;
-    else cout << "1" << endl;
+int main() {
+	int a,b;
+	cin >> a >> b;
+	if(a%b==0) {
+		cout << 0 << endl;
+	}else{
+		cout << 1 << endl;
+	}
 }
+
