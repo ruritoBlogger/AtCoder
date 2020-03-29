425c926bb76ee4d303645a06463895d33d925d1b
rurito <rurito.blog@gmail.com>
2020-03-29 15:42:03 +0900

    updated

---
+++ 95_A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define INF_LL 1LL << 60
+#define INF 99999999
+#define MOD (ll)1000000007
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define REP(i, a, n) for(int i = a; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+void put_double(double obj){printf("%.12f\n",obj);};
+
+template < typename T > std::string to_string( const T& n )
+{
+    std::ostringstream stm ;
+    stm << n ;
+    return stm.str() ;
+}
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+
+int main()
+{
+    string s;
+    cin >> s;
+    
+    int ans = 700;
+    rep(i,3)
+    {
+        if( s[i] == 'o' ) ans += 100;
+    }
+    cout << ans << endl;
+}
---
+++ 95_B.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define INF_LL 1LL << 60
+#define INF 99999999
+#define MOD (ll)1000000007
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define REP(i, a, n) for(int i = a; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+
+void put_double(double obj){printf("%.12f\n",obj);};
+
+template < typename T > std::string to_string( const T& n )
+{
+    std::ostringstream stm ;
+    stm << n ;
+    return stm.str() ;
+}
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+
+int main()
+{
+    int N,X;
+    cin >> N >> X;
+
+    vector<int> m(N);
+    int ans = 0;
+
+    rep(i,N)
+    {
+        cin >> m[i];
+        X -= m[i];
+        ans++;
+    }
+
+    std::vector<int>::iterator iter = std::min_element(all(m));
+    size_t index = std::distance(m.begin(), iter);
+
+    while( X >= m[index] )
+    {
+        X -= m[index];
+        ans++;
+    }
+    cout << ans << endl;
+
+
+
+
+
+}
