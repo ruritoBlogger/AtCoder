e9a26def3e7a87bfc4a7c4f71bd91e3b1dce9dca
rurito <rurito.blog@gmail.com>
2020-04-19 17:45:09 +0900

    updated

--- abc068/c/main.cpp
+++ abc068/c/main.cpp
@@ -21,8 +21,60 @@ template < typename T > std::string to_string( const T& n )
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 
+#define MAX_V (ll)2e6+1
+
+struct edge {
+    ll to;
+    ll cost;
+};
+
+// <最短距離, 頂点の番号>
+#define P pair<ll, ll>
+
+// Vは頂点数
+ll V;
+vector<edge> G[MAX_V];
+vector<ll> d(MAX_V, INF_LL);
+
+void dijkstra(ll s) {
+    priority_queue<P, vector<P>, greater<P> > que;
+    d[s] = 0;
+    que.push(P(0, s));
+
+    while (!que.empty()) {
+        P p = que.top();
+        que.pop();
+        ll v = p.second;
+        if (d[v] < p.first) continue;
+
+        for (ll i=0; i<G[v].size(); ++i) {
+            edge e = G[v][i];
+            if (d[e.to] > d[v] + e.cost) {
+                d[e.to] = d[v] + e.cost;
+                que.push(P(d[e.to], e.to));
+            }
+        }
+    }
+}
+
 
 int main()
 {
+    ll N,M;
+    cin >> N >> M;
+    
+    rep(i,M)
+    {
+        ll a,b;
+        cin >> a >> b;
+        edge e = {a, 1};
+        G[b].push_back(e);
+        e = {b, 1};
+        G[a].push_back(e);
+    }
 
+    dijkstra(1);
+
+    if( d[N] < 3 ) cout << "POSSIBLE" << endl;
+    else cout << "IMPOSSIBLE" << endl;
 }
--- abc129/d/main.cpp
+++ abc129/d/main.cpp
@@ -3,42 +3,72 @@ using namespace std;
 
 #define ll long long
 #define INF 99999999
+#define INF_LL 1LL << 60
+#define MOD (ll)1000000007
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rrep(i, n) for(int i = (int)(n); i >= 0; i--)
+#define REP(i, a, n) for(int i = a; i < (int)(n); i++)
+#define RREP(i, a, n) for(int i = (int)(n); i >= a; i--)
 #define all(x) (x).begin(),(x).end()
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+void put_double(double obj){printf("%.12f\n",obj);};
+
+template < typename T > std::string to_string( const T& n )
+{
+    std::ostringstream stm ;
+    stm << n ;
+    return stm.str() ;
+}
 
 int main()
 {
     int H,W;
     cin >> H >> W;
+
     vector<string> s(H);
-    vector<string> wideS(W);
-    vector<pair<int, int> > pos;
-    vector<pair<int, int> > widePos;
+    rep(i,H) cin >> s[i];
+
+    vector<vector<ll> >width_dp(H, vector<ll>(W,0) );
+    vector<vector<ll> >high_dp(H, vector<ll>(W,0) );
+
     rep(i,H)
     {
-        cin >> s[i];
-        pos.push_back(make_pair(count(all(s[i]),'#'), i));
-        rep(j,s[i].size())wideS[j].push_back(s[i][j]);
-    }
-    sort(all(pos));
-    rep(i,H) cout << pos[i].first << " at " << pos[i].second << endl;
-    cout << endl;
-    rep(i,W) cout << wideS[i] << endl;
-    cout << endl;
-    rep(i,W)
-    {
-        widePos.push_back(make_pair(count(all(wideS[i]),'#'),i));
-    }
-    sort(all(widePos));
-    rep(i,W) cout << widePos[i].first << " at " << widePos[i].second << endl;
-    int ans = 0;
-    if(pos[0].first == 0 && widePos[0].first == 0) cout << H+W-1 << endl;
-    /*
-    else if(pos[0].first == 0)
-    {
-        rep(i,widePos.size())
+        rep(j,W)
         {
-            j
-*/
+            if( s[i][j] != '#' )
+            {
+                if( j > 0 && width_dp[i][j-1] != 0 ) chmax(width_dp[i][j], width_dp[i][j-1]);
+                else
+                {
+                    int tmp_j = j;
+                    for( tmp_j; tmp_j < W; tmp_j++ )
+                    {
+                        if( s[i][tmp_j] == '#' ) break;
+                        else width_dp[i][j]++;
+                    }
+                }
 
+                if( i > 0 && high_dp[i-1][j] != 0 ) chmax(high_dp[i][j], high_dp[i-1][j]);
+                else
+                {
+                    int tmp_i = i;
+                    for( tmp_i; tmp_i < H; tmp_i++ )
+                    {
+                        if( s[tmp_i][j] == '#' ) break;
+                        else high_dp[i][j]++;
+                    }
+                }
+            }
+        }
+    }
+
+    ll ans = 0;
+    rep(i,H)
+    {
+        rep(j, W) chmax(ans, high_dp[i][j] + width_dp[i][j]-1);
+    }
+    cout << ans << endl;
 }
